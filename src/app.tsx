f5d56c1021b48c064cf69437c9d15cb3f08b5318
chansyawn <chansyawn@gmail.com>
2026-04-02 21:59:50 +0800

    chore: initialize tanstack router

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,16 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+// Import the generated route tree
+import { routeTree } from "./route-tree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 export function App() {
-  return <>Hello FluxNote!</>;
+  return <RouterProvider router={router} />;
 }
