cc5f2217fb1012dd84b36ebb4f9c6e0a845ca0b2
chansyawn <chansyawn@gmail.com>
2026-04-03 20:35:13 +0800

    chore: initialize shadcn ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { Button } from "@/ui/components/button";
+
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello FluxNote</div>;
+  return (
+    <div>
+      <Button>Hello FluxNote</Button>
+    </div>
+  );
 }
