e42852f93e3caedaaa5d887f53f6927e963e35f6
chansyawn <chansyawn@gmail.com>
2026-04-04 22:42:47 +0800

    feat: add backend greeting sample

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 import { DirectionStateProvider } from "@/app/direction";
 import { I18nStateProvider } from "@/app/i18n";
+import { queryClient } from "@/app/query";
 import { ThemeStateProvider } from "@/app/theme";
 import { routeTree } from "@/route-tree.gen";
 
@@ -17,9 +19,11 @@ export function App() {
   return (
     <ThemeStateProvider>
       <I18nStateProvider>
-        <DirectionStateProvider>
-          <RouterProvider router={router} />
-        </DirectionStateProvider>
+        <QueryClientProvider client={queryClient}>
+          <DirectionStateProvider>
+            <RouterProvider router={router} />
+          </DirectionStateProvider>
+        </QueryClientProvider>
       </I18nStateProvider>
     </ThemeStateProvider>
   );
